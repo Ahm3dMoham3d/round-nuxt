e921c9a7a0d957fc47c16fdccdaf6868978ed8e1
Ahmed Mohamed <116577902+Ahm3dMoham3d@users.noreply.github.com>
2024-11-24 07:09:45 +0200

    navbar

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,46 @@
 <template>
-  <nav class="bg-white/11 backdrop-blur-[3px] brightness-[.8]">
-    <div class="max-w-[1440px] mx-auto px-8 py-4"></div>
+  <nav class="absolute z-10 top-0 w-full bg-black/20 backdrop-blur-[3px]">
+    <div class="container mx-auto px-4 py-4">
+      <div class="flex items-center justify-between">
+        <div class="flex items-center gap-2">
+          <NuxtLink v-for="(icon, index) in icons" :key="index" to="/">
+            <div
+              class="border border-white rounded-full p-1.5 w-6 h-6 flex items-center justify-center"
+            >
+              <Icon :name="`bi:${icon}`" class="text-white" />
+            </div>
+          </NuxtLink>
+        </div>
+
+        <LangSwitcher />
+      </div>
+      <hr class="border-white my-4" />
+      <div class="flex items-center justify-between">
+        <img src="/public/assets/images/logo.png" />
+        <div class="flex items-center gap-6">
+          <NuxtLink
+            class="text-sm text-white"
+            v-for="(link, index) in links"
+            :key="index"
+            to="/"
+          >
+            {{ link }}
+          </NuxtLink>
+        </div>
+        <UiButton />
+      </div>
+    </div>
   </nav>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const icons = ["twitter-x", "instagram", "tiktok", "snapchat"];
+const links = [
+  "الرئيسية",
+  "خدماتنا",
+  "عن الشركة",
+  "الوجهات السياحية",
+  "المدونة",
+  "ألبوم الصور",
+];
+</script>
 <style></style>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,14 @@
-<template lang="">
-  <section class="hero h-[80vh] bg-no-repeat bg-cover bg-center"></section>
+<template>
+  <section class="h-[90vh] bg-no-repeat bg-cover bg-center">
+    <video
+      class="h-[100%] w-full object-cover"
+      src="/public/assets/videos/hero.mp4"
+      muted
+      loop
+      autoplay
+    ></video>
+    <div class="absolute top-0 w-full h-[90vh]">qgwr</div>
+  </section>
 </template>
 <script>
 export default {};
